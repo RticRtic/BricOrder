11f3f1512823ffbb7d2f28c50ef835d5db6d8e65
RticRtic <90681250+RticRtic@users.noreply.github.com>
2023-11-15 20:08:44 +0100

    AlertDialog

--- app/src/main/java/com/example/bricorder/orders/composables/DefaultAlertDialog.kt
+++ app/src/main/java/com/example/bricorder/orders/composables/DefaultAlertDialog.kt
@@ -1,13 +1,18 @@
 package com.example.bricorder.orders.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun DefaultAlertDialog(
@@ -17,25 +22,26 @@ fun DefaultAlertDialog(
     onConfirm: () -> Unit,
     icon: ImageVector,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-
-    ) {
-        AlertDialog(
-            icon = { Icon(icon, contentDescription = null) },
-            title = { Text(text = title) },
-            text = { Text(text = message) },
-            onDismissRequest = { onDismiss() },
-            confirmButton = {
-                TextButton(onClick = {onConfirm()}) {
-                    Text(text = "Confirm")
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = {onDismiss()}) {
-                    Text(text = "Dismiss")
-                }
+    AlertDialog(
+        icon = { Icon(icon, contentDescription = null) },
+        title = { Text(text = title) },
+        text = {
+            Text(
+                text = message,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text(text = "Delete")
             }
-        )
-    }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text(text = "Dismiss")
+            }
+        },
+    )
 }
